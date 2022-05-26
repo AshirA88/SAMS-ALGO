05e5e16dae83d9f840691d51935c90d34efb8e36
Atharva Gulhane <45367317+AtharvaGulhane@users.noreply.github.com>
2022-05-26 20:39:51 +0530

    Final

--- convert.py
+++ convert.py
@@ -6,87 +6,63 @@ from mysql.connector import (connection)
 import os
 import platform
 
-from datetime import datetime
+from datetime import datetime #for date and time
 
-from numpy import insert #for date
+from numpy import insert
 
+# for connecting to database
 db = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
+  password="root",
   database="sams"
 )
 
-
 curr_date = date.today()        #date
-print(curr_date)
+print("Curent Date :=",curr_date)
 
 now = datetime.now()            #time
 curr_time = now.strftime("%H:%M:%S")
-print("Current Time =", curr_time)
-
-
-
-
-Cursor1 = db.cursor()
-Cursor2 = db.cursor()
-Cursor3 = db.cursor()
-Cursor4 = db.cursor()
-Cursor5 = db.cursor()
-query = ("SELECT id,attendee,timestamp,date FROM record")
+print("Current Time :=", curr_time)
+#Cursors for interacting with Database and Buffered=True is to avoid Lazy Load from Database
+Cursor1 = db.cursor(buffered=True)
+Cursor2 = db.cursor(buffered=True)
+Cursor3 = db.cursor(buffered=True)
+Cursor4 = db.cursor(buffered=True)
+Cursor5 = db.cursor(buffered=True)
+query = ("SELECT id,attendee,timestamp,date FROM record") #fetch data from record
 Cursor1.execute(query)
-
-
 for (id,attendee,timestamp,date) in Cursor1:
-      print("                                                                   ")
+  #To check if Data is already updated
+      if(date!=curr_date): #check if date is similar
+        check = 1
       if(date==curr_date):
+        print("Attendance Already Updated for ", curr_date)
         exit(0)
-      else:
-        max_id = ("SELECT * FROM fpstore")
-        Cursor2.execute(max_id)
-
-        max_value = None
-        for (roll,id,hash) in Cursor2:
-          if (max_value is None or roll > max_value):
-            max_value = roll
-          for i in range(0,max_value):
-            curr_date = date.today()
-            insert = ("INSERT INTO record(id, attendee, timestamp, date) VALUES(%s, %s, %s, %s)")        
-            record = (i, 0, curr_time, curr_date)
-            Cursor3.execute(insert, record)
-          db.commit()
-        print(max_value)
-
-
-
-
-        query = ("SELECT rollno,timestamp,date FROM attendance")
-        Cursor4.execute(query)
-
-
-
-
-
-
-
-        for (rollno,timestamp,date) in Cursor4:
-            print("                                                                   ")
-            if(date==curr_date):
-              attendee = 1
-              insert = ("UPDATE record SET id = %s, attendee = %s, timestamp = %s, date = %s WHERE id = %s")        
-              record = (rollno, attendee, timestamp, date, rollno)
-              Cursor5.execute(insert, record)
-              db.commit()
-
-
-'''
-query = ("SELECT * FROM record")
-Cursor.execute(query)
-
-for (id, Attende,Timestamp) in Cursor:
-    print("                                                                   ")
-    print("id :",id)
-    print("Attende :",Attende)
-    print("Timestamp: ",Timestamp)
-    print("\n")'''
+#If data doesn't exist the new data for current date based attendance is inserted
+if(check==1):
+      max_id = ("SELECT * FROM fpstore")
+      Cursor2.execute(max_id)
+      max_value = None
+      for (roll,id,hash) in Cursor2:
+        if (max_value is None or roll > max_value):
+          max_value = roll        
+      for i in range(0,max_value+1):
+        curr_date = date.today()
+        insert = ("INSERT INTO record(id, attendee, timestamp, date) VALUES(%s, %s, %s, %s)")  
+        record = (i, 0, curr_time, curr_date)
+        Cursor3.execute(insert, record)
+      db.commit()
+      query = ("SELECT rollno,timestamp,date FROM attendance")
+      Cursor4.execute(query)
+#If Data is inserted the attendance has to be fetched from the attendance table and updated to record table
+      for (rollno,timestamp,date) in Cursor4:          
+          if(date==curr_date):
+            attendee = 1
+            insert = ("UPDATE record SET id = %s, attendee = %s, timestamp = %s, date = %s WHERE id = %s")        
+            record = (rollno, attendee, timestamp, date, rollno)
+            Cursor5.execute(insert, record)
+            db.commit()
+      print("Updation Completed")
+      quit()
     
